135bc5dc53bcb6e4a5e82a9525ebdc292e752c3b
REMO-hack <patel.balveer07@gmail.com>
2024-11-12 10:38:43 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
+import java.io.*;
 import java.util.*;
-public static void main(String args[])
-  {
-  Scanner sc = new Scanner(System.in);
-  int t = 
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+Scanner sc = new Scanner(System.in);
+    int t = sc.nextInt();
+        while(t-->0){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            if(c>a+b || a>b+c || b>c+a) System.out.println("YES");
+            else System.out.println("NO");
+        }
+    }
+}
